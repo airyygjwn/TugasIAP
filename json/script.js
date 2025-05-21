badd886fe5f9f5ecee3bb6285fd57a19cf2f3f4a
Nelsi <nlsrmnda@gmail.com>
2025-05-21 22:52:06 +0700

    commit 5

--- json/script.js
+++ json/script.js
@@ -1,7 +1,22 @@
-let mahasiswa =  {
-    nama : "Nelsi Ramanda",
-    nim : "2217020003",
-    email : "nlsrmnda@gmail.com"
-}
+// let mahasiswa =  {
+//     nama : "Nelsi Ramanda",
+//     nim : "2217020003",
+//     email : "nlsrmnda@gmail.com"
+// }
 
-console.log(JSON.stringify(mahasiswa));
+// console.log(JSON.stringify(mahasiswa));
+// ini adalah cara mengubah objek ke Json
+
+let xhr = new XMLHttpRequest();
+xhr.onreadystatechange = function () {
+    if(xhr.readyState == 4 && xhr.status == 200) {
+        let mahasiswa = JSON.parse(this.responseText);
+        console.log(mahasiswa);
+    }
+}
+xhr.open('GET', 'coba.json', true);
+xhr.send();
+
+$.getJSON('coba.json', function (data){
+    console.log(data);
+});
