5d8940fa59848f48a56512a2e6625d7decddde24
Nelsi <nlsrmnda@gmail.com>
2025-05-24 13:21:06 +0700

    commit 8

---
+++ Studi_Kasus/js/script.js
@@ -0,0 +1,63 @@
+function tampilkanSemuaMenu() {
+    $('#daftar-menu').html('');
+    $.getJSON('Data/pizza.json', function(data) {
+        let menu = data.menu;
+        $.each(menu, function(i, data) {
+            $('#daftar-menu').append(`
+                <div class="col-md-4">
+                    <div class="card mb-3">
+                        <img src="img/menu/${data.gambar}" class="card-img-top">
+                        <div class="card-body">
+                            <h5 class="card-title">${data.nama}</h5>
+                            <p class="card-text">${data.deskripsi}</p>
+                            <h5 class="card-title">Rp. ${data.harga}</h5>
+                            <a href="#" class="btn btn-primary">Pesan Sekarang</a>
+                        </div>
+                    </div>
+                </div>
+            `);
+        });
+    });
+}
+
+tampilkanSemuaMenu();
+
+$('.nav-link').on('click', function(e) {
+    e.preventDefault(); // Cegah reload karena href="#"
+
+    $('.nav-link').removeClass('active');
+    $(this).addClass('active');
+
+    let kategori = $(this).data('kategori'); // Ambil dari atribut data-kategori
+    $('h1').html($(this).text().trim());     // Tampilkan di heading
+
+    if (kategori === 'all menu') {
+        tampilkanSemuaMenu();
+        return;
+    }
+
+    $.getJSON('Data/pizza.json', function(data) {
+        let menu = data.menu;
+        let content = '';
+
+        $.each(menu, function(i, data) {
+            if (data.kategori === kategori) {
+                content += `
+                    <div class="col-md-4">
+                        <div class="card mb-3">
+                            <img src="img/menu/${data.gambar}" class="card-img-top">
+                            <div class="card-body">
+                                <h5 class="card-title">${data.nama}</h5>
+                                <p class="card-text">${data.deskripsi}</p>
+                                <h5 class="card-title">Rp. ${data.harga}</h5>
+                                <a href="#" class="btn btn-primary">Pesan Sekarang</a>
+                            </div>
+                        </div>
+                    </div>
+                `;
+            }
+        });
+
+        $('#daftar-menu').html(content);
+    });
+});
